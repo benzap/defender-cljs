6ad275d6734dbb672f542de64f11b2d09f19fe17
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-11-27 22:50:00 -0500

    basic working render

--- src/cljs/defender_cljs/canvas/core.cljs
+++ src/cljs/defender_cljs/canvas/core.cljs
@@ -5,43 +5,45 @@
 (def width (.-clientWidth dom))
 (def height (.-clientHeight dom))
 
+(def left-bound)
+
 (def renderer (THREE.WebGLRenderer.))
 (.setSize renderer width height)
 
-
-
-(def camera (THREE.OrthographicCamera. -1000 1000
-                                       -1000 1000
-                                       -100 100))
-(aset (-> camera .-position) "z" -5)
-
-(def light (THREE.AmbientLight. 0x404040))
-
-(defn init [dom])
-
-(defn animate [dom])
+(def camera (THREE.OrthographicCamera. 0 1000
+                                       0 (* 1000 (/ 3 4))
+                                       -1 1))
+(aset (-> camera .-position) "z" 0)
 
 (defn square [x y width height
               & {:keys [color]
                  :or {color 0xaaffaa}}]
   (let [geometry (THREE.BoxGeometry. width height 1)
-        material (THREE.MeshBasicMaterial. #js {:color color})]
-    (new js/THREE.Mesh geometry material)))
-
-
+        material (THREE.MeshBasicMaterial. #js {:color color})
+        mesh (THREE.Mesh. geometry material)]
+    (aset (-> mesh .-position) "x" x)
+    (aset (-> mesh .-position) "y" y)
+    mesh))
 
 (def main-scene (THREE.Scene.))
+
+(def spinning-square (square 200 200 200 200))
+
 (doto main-scene
   (.add camera)
-  (.add light)
-  (.add (square 10 10 100 100))
-  (.add (square 200 200 200 200))
+  (.add (square 0 10 100 100 :color 0x330000))
+  (.add (square 0 100 100 100))
+  (.add spinning-square)
   )
 
-(.log js/console main-scene)
-(.log js/console renderer)
-
 (.appendChild dom (-> renderer .-domElement))
-(.render renderer main-scene camera)
 
+(defn render []
+  (.requestAnimationFrame js/window render)
+  (.render renderer main-scene camera)
+  (animate))
 
+(defn animate []
+  (.rotateOnAxis spinning-square (THREE.Vector3. 0 0 1) 0.01))
+
+(render)
