23002bb680458cf24f5c7211e69c80d961c8fb61
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-11-27 21:47:02 -0500

    have a basic three.js scene working

--- src/cljs/defender_cljs/canvas/core.cljs
+++ src/cljs/defender_cljs/canvas/core.cljs
@@ -1,23 +1,47 @@
 (ns defender-cljs.canvas.core)
 
+(def dom (.getElementById js/document "app"))
 
-(defonce main-scene (THREE.Scene))
+(def width (.-clientWidth dom))
+(def height (.-clientHeight dom))
 
-(defn create-ortho-camera [width height]
-  (THREE.OrthographicCamera 0 width height 0))
+(def renderer (THREE.WebGLRenderer.))
+(.setSize renderer width height)
 
-(defn add-square [x y width height
-                  & {:keys [color]
-                     :or {color 0xaaffaa}}]
-  (let [geometry (new js/THREE.BoxGeometry width height 1)
-        material (new js/THREE.MeshBasicMaterial #js {:color color})]
-    (new js/THREE.Mesh geometry material)))
+
+
+(def camera (THREE.OrthographicCamera. -1000 1000
+                                       -1000 1000
+                                       -100 100))
+(aset (-> camera .-position) "z" -5)
+
+(def light (THREE.AmbientLight. 0x404040))
 
 (defn init [dom])
 
 (defn animate [dom])
 
-(defn cube []
-  (let [geometry (new js/THREE.BoxGeometry 1 1 1)
-        material (new js/THREE.MeshBasicMaterial #js {:color 0xaaffaa})]
+(defn square [x y width height
+              & {:keys [color]
+                 :or {color 0xaaffaa}}]
+  (let [geometry (THREE.BoxGeometry. width height 1)
+        material (THREE.MeshBasicMaterial. #js {:color color})]
     (new js/THREE.Mesh geometry material)))
+
+
+
+(def main-scene (THREE.Scene.))
+(doto main-scene
+  (.add camera)
+  (.add light)
+  (.add (square 10 10 100 100))
+  (.add (square 200 200 200 200))
+  )
+
+(.log js/console main-scene)
+(.log js/console renderer)
+
+(.appendChild dom (-> renderer .-domElement))
+(.render renderer main-scene camera)
+
+
--- src/cljs/defender_cljs/core.cljs
+++ src/cljs/defender_cljs/core.cljs
@@ -1,8 +1,7 @@
 (ns defender-cljs.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
-
-(def app-state (atom {:text "Hello Chest"}))
+            [om.dom :as dom :include-macros true]
+            [defender-cljs.canvas.core :as c]))
 
 (def game-state
   (atom {:stage nil
@@ -11,12 +10,8 @@
          :lives 0
          :player {:position [0 0]}}))
 
-(defn main []
-  (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (:text app-state)))))
-    app-state
-    {:target (.getElementById js/document "app")}))
+(def dom-app (.getElementById js/document "app"))
+
+(def canvas (THREE.WebGLRenderer.))
+
+(defn main [])
