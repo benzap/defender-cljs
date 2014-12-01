6083138337a2182c8af2da00b4ce8d52a9d6b7c1
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-11-30 22:49:42 -0500

    dual 3d canvas model

--- src/cljs/defender_cljs/canvas/core.cljs
+++ src/cljs/defender_cljs/canvas/core.cljs
@@ -15,6 +15,11 @@
                                        -1 1))
 (aset (-> camera .-position) "z" 0)
 
+(def hud-camera (THREE.OrthographicCamera. 0 1000
+                                           0 (* 1000 (/ 3 4))
+                                           -1 1))
+(aset (-> hud-camera .-position) "z" 0)
+
 (defn square [x y width height
               & {:keys [color]
                  :or {color 0xaaffaa}}]
@@ -26,24 +31,31 @@
     mesh))
 
 (def main-scene (THREE.Scene.))
+(def hud-scene (THREE.Scene.))
 
 (def spinning-square (square 200 200 200 200))
 
 (doto main-scene
   (.add camera)
-  (.add (square 0 10 100 100 :color 0x330000))
-  (.add (square 0 100 100 100))
+  (.add (square 50 50 100 100 :color 0x330000))
+  (.add (square 50 100 100 100))
   (.add spinning-square)
   )
 
+(doto hud-scene
+  (.add hud-camera)
+  (.add (square 500 10 1000 20 :color 0x555555)))
+
 (.appendChild dom (-> renderer .-domElement))
+
+(defn animate []
+  (.rotateOnAxis spinning-square (THREE.Vector3. 0 0 1) 0.01))
 
 (defn render []
   (.requestAnimationFrame js/window render)
   (.render renderer main-scene camera)
+  (aset renderer "autoClear" false)
+  (.render renderer hud-scene hud-camera)
   (animate))
-
-(defn animate []
-  (.rotateOnAxis spinning-square (THREE.Vector3. 0 0 1) 0.01))
 
 (render)
