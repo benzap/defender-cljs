7abe72505771b7b428f21bd1cb21617a3b6bdde0
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-12-02 21:53:53 -0500

    started more work on separating things

--- src/cljs/defender_cljs/canvas/hud.cljs
+++ src/cljs/defender_cljs/canvas/hud.cljs
@@ -1,2 +1,8 @@
 (ns defender-cljs.canvas.hud
-  "Provides an abstraction over Three.js for producing hud objects")
+  "Provides an abstraction over Three.js for producing hud objects"
+  (:require [defender-cljs.constants :as c]))
+
+(def hud-map-width (/ c/view-width 3))
+
+(defn draw-hud-layout []
+  (let []))
---
+++ src/cljs/defender_cljs/constants.cljs
@@ -0,0 +1,9 @@
+(ns defender-cljs.constants)
+
+(def map-width 7000)
+
+(def view-width (/ map-width 7))
+
+(def view-height (* view-width (/ 3 4)))
+
+(def top-overlay-height (/ view-height 5))
