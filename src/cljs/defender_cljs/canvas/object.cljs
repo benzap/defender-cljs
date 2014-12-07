e7dafbd092990f40e598d72ebec347c19926e4a9
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-12-07 14:06:27 -0500

    fixed sprites

--- src/cljs/defender_cljs/canvas/object.cljs
+++ src/cljs/defender_cljs/canvas/object.cljs
@@ -5,9 +5,10 @@
 
 (defn translate!
   "Translate Object3D"
-  [obj x y]
+  [obj x y & [z]]
   (.translateX obj x)
-  (.translateY obj y))
+  (.translateY obj y)
+  (.translateZ obj (or z 0)))
 
 (defn set-position!
   "Set the position of the Object3D"
@@ -31,3 +32,21 @@
   (aset (-> obj .-rotation) "y" 0)
   (aset (-> obj .-rotation) "z" ang)
   obj)
+
+(defn scale!
+  "Sets the modified scale multiplicative to it's current scale."
+  [obj x y & [z]]
+  (let [scale (aget obj "scale")
+        sc-x (aget scale "x")
+        sc-y (aget scale "y")
+        sc-z (aget scale "z")]
+    (aset scale "x" (* sc-x x))
+    (aset scale "y" (* sc-y y))
+    (aset scale "z" (* sc-z (or z 1)))))
+
+(defn set-scale!
+  "Sets the current scale of the object"
+  [obj x y & [z]]
+  (aset (-> obj .-scale)  "x" x)
+  (aset (-> obj .-scale)  "y" y)
+  (aset (-> obj .-scale)  "z" (or z 1)))
--- src/cljs/defender_cljs/canvas/sprite.cljs
+++ src/cljs/defender_cljs/canvas/sprite.cljs
@@ -1,4 +1,5 @@
-(ns defender-cljs.canvas.sprite)
+(ns defender-cljs.canvas.sprite
+  (:require [defender-cljs.canvas.object :as obj]))
 
 (defn load-texture 
   "Loads the given texture asset, and returns a map to it"
@@ -11,10 +12,18 @@
         "color" 0xffffff
         "fog" true}))
 
+(def default-scale [64 64])
+
 (defn make-sprite 
   "makes a sprite from a given texture-url. The url is based on the
   root folder /assets/"
-  [name]
+  [name & {:keys [scale]
+           :or {scale default-scale}}]
   (let [texture-map (load-texture name)
-        sprite-material (make-sprite-material texture-map)]
-    (THREE.Sprite. sprite-material)))
+        sprite-material (make-sprite-material texture-map)
+        sprite (THREE.Sprite. sprite-material)]
+    ;;The scale of the sprite is 1 by 1, which is 1px by 1px
+    ;;so the scale is fixed at the default-scale, or whatever is supplied
+    (.log js/console "scale" scale)
+    (obj/set-scale! sprite (first scale) (second scale))
+    sprite))
