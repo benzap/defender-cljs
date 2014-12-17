cab6f1822c401befd51d931c40151d8f92bb56ef
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-12-17 15:14:56 -0500

    lazy

--- src/cljs/defender_cljs/events.cljs
+++ src/cljs/defender_cljs/events.cljs
@@ -2,22 +2,21 @@
   (:require-macros [defender-cljs.events :refer [on-keydown on-keyup]]))
 
 (def keycode-map
-  {:left 37
+  {:enter 13
+   :left 37
    :up 38
    :right 39
    :down 40
-   :w 87
-   :a 65
-   :s 83
-   :d 100
    :space 32
-   :enter 13
-   :i 73
-   :j 74
-   :k 75
-   :l 76
-   :b 66
-   :g 71})
+   :a 65 :b 66 :c 67
+   :d 68 :e 69 :f 70
+   :g 71 :h 72 :i 73
+   :j 74 :k 75 :l 76
+   :m 77 :n 78 :o 79
+   :p 80 :q 81 :r 82
+   :s 83 :t 84 :u 85
+   :v 86 :w 87 :x 88
+   :y 89 :z 90})
 
 (def keys-pressed (atom #{}))
 
