37c166d24406a1bd6a3a61eb31dd45e0051676b4
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-12-22 21:59:14 -0500

    white lines, and some work on assets

--- src/cljs/defender_cljs/overlay/core.cljs
+++ src/cljs/defender_cljs/overlay/core.cljs
@@ -7,6 +7,8 @@
 
 (def hud-line-width 5)
 (def hud-text-size 20)
+(def hud-map-line-width 2)
+(def hud-map-line-height 17)
 
 ;;top of the screen overlay. bounds for the map space
 (def left-bound (/ c/hud-width 3))
@@ -57,12 +59,58 @@
          :color line-color
          :line-width line-width)
 
-        white-left-bound
+        ;;hud top white line left side
+        white-left-top-bound
         (geo/draw-line
          [[inner-left-bound top-bound 1]
-          [inner-left-bound bottom-bound 1]]
+          [inner-left-bound (- top-bound hud-map-line-height) 1]]
          :color 0xffffff
-         :line-width line-width)
+         :line-width hud-map-line-width)
+        
+        ;;hud top white line right side
+        white-right-top-bound
+        (geo/draw-line
+         [[inner-right-bound top-bound 1]
+          [inner-right-bound (- top-bound hud-map-line-height) 1]]
+         :color 0xffffff
+         :line-width hud-map-line-width)
+        
+        ;;hud top white line
+        white-top-bound
+        (geo/draw-line
+         [[inner-left-bound (- top-bound (/ line-width 2)) 1]
+          [inner-right-bound (- top-bound (/ line-width 2)) 1]]
+         :color 0xffffff
+         :line-width (* line-width 2))
+
+        ;;bottom
+
+        ;;hud bottom white line left side
+        white-left-bottom-bound
+        (geo/draw-line
+         [[inner-left-bound bottom-bound 1]
+          [inner-left-bound (+ bottom-bound hud-map-line-height) 1]]
+         :color 0xffffff
+         :line-width hud-map-line-width)
+        
+        ;;hud bottom white line right side
+        white-right-bottom-bound
+        (geo/draw-line
+         [[inner-right-bound bottom-bound 1]
+          [inner-right-bound (+ bottom-bound hud-map-line-height) 1]]
+         :color 0xffffff
+         :line-width hud-map-line-width)
+        
+        ;;hud bottom white line
+        white-bottom-bound
+        (geo/draw-line
+         [[(- inner-left-bound (/ hud-map-line-width 3))
+           (+ bottom-bound (/ line-width 2)) 1]
+          [(+ inner-right-bound (/ hud-map-line-width 3))
+           (+ bottom-bound (/ line-width 2)) 1]]
+         :color 0xffffff
+         :line-width (* line-width 2))
+
         
         ]
     
@@ -70,7 +118,12 @@
     (.add scene left-line)
     (.add scene right-line)
     (.add scene top-line)
-    (.add scene white-left-bound)))
+    (.add scene white-left-top-bound)
+    (.add scene white-right-top-bound)
+    (.add scene white-top-bound)
+    (.add scene white-left-bottom-bound)
+    (.add scene white-right-bottom-bound)
+    (.add scene white-bottom-bound)))
 
 (def score (text/create-label "000000" {:size 20 :color 0xff00ff}))
 (obj/set-position! score 120 (- c/hud-height c/top-overlay-height
